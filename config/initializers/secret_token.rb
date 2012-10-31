6c02e7b1851382cdda2ddead66177c3a07ac7455
najum <n.ali@isys-software.de>
2012-10-31 22:38:39 +0100

    init new rails project (empty)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ClickAndEat::Application.config.secret_token = 'a3d450cf1990e8b8ef0e278a41ec04a741dfd6872f2fee75d8a1b6db841404cfbe937f7bf2b306c81e93ebddba1488ce10f6f942805ff977233674a907247df4'
