f04dd8038bafdd60c07957a24180db2d1496bb6a
najum <n.ali@isys-software.de>
2012-12-20 15:24:08 +0100

    missing stylesheet

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
   def create
     user_account = UserAccount.find_by_email(params[:email])
     if user_account && user_account.authenticate(params[:password])
+      reset_session
       session[:user_account_id] = user_account
       redirect_to root_path, notice: "You are logged in"
     else
